06fdc08dd62fbef712e52f50ac186b98e1dd0ff2
Chris Malliapi <chris@malliapi.co.uk>
2020-05-08 14:37:59 +0100

    publish

--- infrastructure/githubpackages.sh
+++ infrastructure/githubpackages.sh
@@ -2,3 +2,4 @@
 set -x
 echo -e "@malliapi:registry=https://npm.pkg.github.com/" >> .npmrc
 echo -e "_auth=$GITHUB_PUBLISH_TOKEN\nemail=$GITHUB_PUBLISH_EMAIL\nalways-auth=true" >> .npmrc
+npm set @malliapi:registry https://npm.pkg.github.com
