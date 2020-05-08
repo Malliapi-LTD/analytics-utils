2ecfc6cd6168dd49448798445f1fd11d03831dc0
Chris Malliapi <chris@malliapi.co.uk>
2020-05-08 15:20:43 +0100

    publish

--- infrastructure/githubpackages.sh
+++ infrastructure/githubpackages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x
 echo -e "@$GITHUB_PUBLISH_SCOPE:registry=https://npm.pkg.github.com/" >> .npmrc
-echo -e "_auth=$GITHUB_PUBLISH_TOKEN\nemail=$GITHUB_PUBLISH_EMAIL\nalways-auth=true" >> .npmrc
+echo -e "_authToken=$GITHUB_PUBLISH_TOKEN\nemail=$GITHUB_PUBLISH_EMAIL\nalways-auth=true" >> .npmrc
 npm set @$GITHUB_PUBLISH_SCOPE:registry https://npm.pkg.github.com
